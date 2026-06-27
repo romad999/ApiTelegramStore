3d75859cbe166874e61dd50d99162b97b57710a7
romanramazanov <ramazanovroman1919@gmail.com>
2026-06-27 20:43:39 +0500

    Integrate Liquibase database migrations using SQL format

---
+++ src/main/resources/db/changelog/db.changelog-master.sql
@@ -0,0 +1,9 @@
+--Liquibase formatted sql
+
+--changeset Roma:1
+CREATE TABLE products (
+                          id BIGSERIAL PRIMARY KEY,
+                          title VARCHAR(255) NOT NULL,
+                          price NUMERIC(10,2),
+                          quantity INT
+);
